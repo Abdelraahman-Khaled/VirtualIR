36a0bc0454bef2f95131fb3ce7cb38643b6140eb
Abdelrahman Khaled <abodzoro10@gmail.com>
2024-09-16 17:02:41 +0300

    add:deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      host: "localhost",
-      Port: 8011,
-    },
-    base: "/VirtualIR",
-  },
+  base: "/VirtualIR/",
 });
